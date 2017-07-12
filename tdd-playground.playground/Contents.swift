32c0006428904fdb6ff6c8ccc7c45ee45efb5c84
Alexey Fayzullov <alex.f@techery.io>
2017-07-12 15:41:44 +0300

    Initial commit

---
+++ tdd-playground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import XCTest
+import Foundation
+import UIKit
+
+class MySuite: XCTestCase {
+  func testSomeLogic() {
+    XCTAssert(false)
+  }
+}
+
+MySuite.defaultTestSuite().run()
